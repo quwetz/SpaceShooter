72fd4ea121004064740d247dff752757e14131fd
quwetz <davidu144@gmail.com>
2018-12-13 22:10:11 +0100

    Asteroid added

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+		rb.velocity = new Vector3 (0, 0, -speed);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Projectile") {
+			Destroy (other.gameObject);
+			Destroy (gameObject);
+		} else if (other.tag == "Player") {
+			Destroy (other.gameObject);
+		}
+	}
+}
