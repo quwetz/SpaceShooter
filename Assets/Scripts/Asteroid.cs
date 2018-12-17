5dce06705e0309dcc35b5b6db500f909662fc6f0
quwetz <davidu144@gmail.com>
2018-12-17 20:02:51 +0100

    Asteroid ein bisschen an das Tutorial angepasst

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -7,29 +7,38 @@ using UnityEngine;
 public class Asteroid : BrauchtGameController {
 
     //Die Geschwindigkeit mit der die Asteroiden fliegen
-	public float geschwindigkeit;
+    public float geschwindigkeit;
+
+    //Rotationsgeschwindigkeit
+    public float rotationsGeschwindigkeit;
 
     //Der Sound der bei der Zerstörung eines Asteroiden abgespielt wird.
-    public AudioClip explosionsSound;
+    //  public AudioClip explosionsSound;
 
     //Referenz auf den Rigidbody
-    private Rigidbody rb;
+     private Rigidbody rb;
 
 
 
 
     //Start() wird von Unity aufgerufen, wenn das Object erzeugt wird.
     void Start(){
+
+
+
         //Setze die Referenz auf den Rigidbody
-		rb = GetComponent<Rigidbody> ();
+        rb = GetComponent<Rigidbody> ();
+
+        //SetzeGameControllerReferenz();
 
         //Setze die Geschwindigkeit
-		rb.velocity = new Vector3 (0, 0, -geschwindigkeit);
-        
-        //Diese Funktion wird aus der Parent-Klasse "BrauchtGameController" geerbt.
-        SetzeGameControllerReferenz();
-    }
+        rb.velocity = new Vector3 (0, 0, -geschwindigkeit);
 
+        rb.angularVelocity = Random.insideUnitSphere * rotationsGeschwindigkeit;
+
+        
+    }
+/*
     //OnTriggerEnter() wird von Unity aufgerufen, wenn ein anderer Collider mit dem eigenen Collider kollidiert.
     void OnTriggerEnter(Collider other)
     {
@@ -49,5 +58,5 @@ public class Asteroid : BrauchtGameController {
             //Lösche den Asteroiden selbst
             Destroy(gameObject);
         }
-    }
+    } */
 }
